d685605f7ec51e630a2bbe8dcc09273cc32ff897
Felipe Guimarães <felipe700guima@gmail.com>
2023-01-27 13:25:27 -0300

    colocando e configurando o projeto react para a execução

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Github Finder</h1>
+      <Outlet />
+    </div>
+  )
+}
+
+export default App
