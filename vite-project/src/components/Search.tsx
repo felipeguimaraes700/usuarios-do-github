5997e5e940a9b560b3c7629787f1eaf76a54afa4
Felipe Guimarães <felipe700guima@gmail.com>
2023-01-27 15:54:14 -0300

    função que pega usuários do git hub

---
+++ vite-project/src/components/Search.tsx
@@ -0,0 +1,26 @@
+type SearchProps = {
+  loadUser: (username: string) => Promise<void>
+}
+
+import { useState } from 'react'
+
+import {BsSearch} from 'react-icons/bs';
+
+const Search = ({loadUser}: SearchProps) => {
+  const [userName, setUserName] = useState("");
+
+  return (
+    <div>
+      <h2>Busque por um usuário:</h2>
+      <p>conheça seus melhores repositórios</p>
+      <div>
+        <input type="text" value="" placeholder='Digite o nome do usuário' onChange={(e) => setUserName(e.target.value)}/>
+        <button onClick={()=>loadUser(userName)}>
+          <BsSearch />
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Search
